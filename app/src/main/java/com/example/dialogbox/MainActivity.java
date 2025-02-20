6282ad16d92261f921761c857c74c56536d05a65
HarshaJE <harshaje69@gmail.com>
2025-02-21 00:40:43 +0530

    Added two buttons for Dialog Box and Toast. Made them pretty. Coded Dialog box and I made Toast.

--- app/src/main/java/com/example/dialogbox/MainActivity.java
+++ app/src/main/java/com/example/dialogbox/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.dialogbox;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.DiBo); //Connect "b1" object to button "DiBo" in XML
+
+        Button b2 = findViewById(R.id.ToBu); //Connect "b2" object to button "ToBu" in XML
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                showDialog();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "You clicked on Toast", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
+    private void showDialog() {
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        b.setTitle("Dialog Box");
+        b.setMessage("This is a sample dialog");
+        b.setPositiveButton("KOKO", new DialogInterface.OnClickListener(){
+           @Override
+           public void onClick(DialogInterface d, int w){
+               d.dismiss();
+           }
+        });
+
+        b.setNegativeButton("NO KOKO", new DialogInterface.OnClickListener(){
+           public void onClick(DialogInterface d, int w){
+               d.dismiss();
+           }
+        });
+
+        AlertDialog c = b.create();
+        c.show();
+    }
+
 }
